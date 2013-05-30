f2a08b798fe7ab626e00917ba9bb1d252e072d56
tomykaira <tomykaira@gmail.com>
2013-05-30 16:10:29 +0900

    Extract methods

--- receiver_initiated.c
+++ receiver_initiated.c
@@ -24,6 +24,8 @@ void wait_complete(resource_t *res, int cq_flag);
 
 #define TCP_PORT 8352
 
+#define POLL(x) while (x == 0) { sched_yield(); }
+
 const int request_size = 256;
 
 void display_received(char * received, int length)
@@ -42,6 +44,19 @@ void display_received(char * received, int length)
   printf("\n");
 }
 
+static void format_mr(struct ibv_mr *mr, char *output)
+{
+  INT_TO_BE(output, (unsigned int)mr->rkey);
+  INT_TO_BE(output + 4, (((uintptr_t)mr->addr) >> 32));
+  INT_TO_BE(output + 8, (((uintptr_t)mr->addr) & 0xffffffff));
+}
+
+static void extract_mr(char *input, uint32_t *ptr_key, uint64_t *ptr_addr)
+{
+  *ptr_key  = BE_TO_INT(input);
+  *ptr_addr = ((uint64_t)BE_TO_INT(input + 4) << 32) | BE_TO_INT(input + 8);
+}
+
 /* addr(8), rkey(4), len(1), key(242), check(1) -> 256 */
 static void format_receive_request(char * key,
                                    size_t keylen,
@@ -50,12 +65,36 @@ static void format_receive_request(char * key,
 {
   assert(keylen <= 242);
   memset(output, 0, 256);
-  INT_TO_BE(output, (unsigned int)mr->rkey);
-  INT_TO_BE(output + 4, (((uintptr_t)mr->addr) >> 32));
-  INT_TO_BE(output + 8, (((uintptr_t)mr->addr) & 0xffffffff));
+  format_mr(mr, output);
   output[12] = (uint8_t)keylen;
   memcpy(output + 13, key, keylen);
   output[255] = 1;
+}
+
+static void extract_receive_request(char *request,
+                                    char *key,
+                                    size_t *keylen,
+                                    uint32_t *ptr_key,
+                                    uint64_t *ptr_addr)
+{
+  size_t len;
+  extract_mr(request, ptr_key, ptr_addr);
+  *keylen = len = (size_t)request[12];
+  memcpy(key, request + 13, len);
+}
+
+void prepare_rdma_write_wr(struct ibv_mr *mr, struct ibv_sge *sge, struct ibv_send_wr *wr)
+{
+  memset(wr,  0, sizeof(*wr));
+  memset(sge, 0, sizeof(*sge));
+
+  sge->addr   = (intptr_t)mr->addr;
+  sge->length = mr->length;
+  sge->lkey   = mr->lkey;
+
+  wr->sg_list = sge;
+  wr->num_sge = 1;
+  wr->opcode  = IBV_WR_RDMA_WRITE;
 }
 
 static void act_as_sender(resource_t *res)
@@ -76,55 +115,31 @@ static void act_as_sender(resource_t *res)
   TEST_NZ(request_mr = ibv_reg_mr(res->pd, request, request_size, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
   memset(request, 0, request_size);
 
-  INT_TO_BE(buf, request_mr->rkey);
-  INT_TO_BE(buf + 4, (((intptr_t)request_mr->addr) >> 32));
-  INT_TO_BE(buf + 8, (((intptr_t)request_mr->addr) & 0xffffffff));
-
-  DEBUG {printf("key(%u) addr(%lx)\n", (uint)request_mr->rkey, (uint64_t)request_mr->addr);}
+  format_mr(request_mr, buf);
 
   TEST_Z( post_ibsend(res, IBV_WR_SEND, &sge_buf, sr, 1, 1) );
 
   TEST_NZ(data_mr = ibv_reg_mr(res->pd, data, RDMA_MIN_SIZE, IBV_ACCESS_LOCAL_WRITE));
-
-  sge.addr = (intptr_t)data;
-  sge.length = RDMA_MIN_SIZE;
-  sge.lkey = data_mr->lkey;
-
-  memset(&wr, 0, sizeof(wr));
-  wr.sg_list = &sge;
-  wr.num_sge = 1;
-  wr.opcode = IBV_WR_RDMA_WRITE;
+  prepare_rdma_write_wr(data_mr, &sge, &wr);
 
   wait_complete(res, SCQ_FLG);
-
 
   while (request[request_size-2] == 0) {
     uint32_t peer_key;
     uint64_t peer_addr;
     char key[242];
-    int key_len;
+    size_t key_len;
 
     DEBUG {printf("waiting request\n");}
 
     /* polling */
-    while (request[request_size-1] == 0) {
-      sched_yield();
-    }
-
-    DEBUG {printf("requested\n");}
-
+    POLL(request[request_size-1]);
     request[request_size-1] = 0;
 
-    peer_key  = BE_TO_INT(request);
-    peer_addr = BE_TO_INT(request + 4);
-    peer_addr = (peer_addr << 32) | BE_TO_INT(request + 8);
-    key_len = (int)request[12];
-    memcpy(key, request + 13, key_len);
+    extract_receive_request(request, key, &key_len, &peer_key, &peer_addr);
 
-    DEBUG {printf("key (%s, %d)\n", key, key_len);}
     TEST_Z(strncmp(key, "test", key_len));
 
-    DEBUG {printf("key (%u) addr (%lx)\n", peer_key, peer_addr);}
     wr.wr.rdma.remote_addr = peer_addr;
     wr.wr.rdma.rkey = peer_key;
 
@@ -166,30 +181,16 @@ static void act_as_receiver(resource_t *res)
   TEST_Z(post_ibreceive(res, &sge_buf, 1));
   wait_complete(res, RCQ_FLG);
 
-  sender_key  = BE_TO_INT(buf);
-  sender_addr = BE_TO_INT(buf + 4);
-  sender_addr = (sender_addr << 32) | BE_TO_INT(buf + 8);
-
-  DEBUG {printf("key(%u) addr(%lx)\n", sender_key, sender_addr);}
+  extract_mr(buf, &sender_key, &sender_addr);
 
   /* prepare request send work */
-  sge.addr = (intptr_t)request;
-  sge.length = request_size;
-  sge.lkey = request_mr->lkey;
-
-  memset(&wr, 0, sizeof(wr));
-  wr.sg_list = &sge;
-  wr.num_sge = 1;
-  wr.opcode = IBV_WR_RDMA_WRITE;
+  prepare_rdma_write_wr(request_mr, &sge, &wr);
 
   wr.wr.rdma.remote_addr = sender_addr;
   wr.wr.rdma.rkey = sender_key;
 
   for (int i = 0; i < 10; ++i) {
     format_receive_request("test", 4, data_mr, request);
-    DEBUG {printf("key (%u) addr (%lx)\n", data_mr->rkey, (uintptr_t)data_mr->addr);}
-
-    /* send request */
     TEST_Z(ibv_post_send(res->qp, &wr, &bad_wr));
 
     /* this can be postponed or interleaved with data poll */
@@ -198,9 +199,7 @@ static void act_as_receiver(resource_t *res)
     DEBUG {printf("waiting response\n");}
 
     /* poll last byte */
-    while (data[RDMA_MIN_SIZE-1] == 0) {
-      sched_yield();
-    }
+    POLL(data[RDMA_MIN_SIZE-1]);
     data[RDMA_MIN_SIZE-1] = 0;
     printf("Received: %s\n", data);
   }
